e704e576113193f73288f7948254db8b244130d8
shaikhfurquan <shaikhfurquan313@gmail.com>
2023-11-01 00:50:23 +0530

    code added

--- shallowCopy.js
+++ shallowCopy.js
@@ -11,14 +11,19 @@ let obj = {
     }
 }
 let user = { ...obj }
-user.address.city = 'High-Tech'     //change original object as well.So we use deep copy
+user.address.city = 'High-Tech'     //change original object as well.So we use deep copy.
 console.log('obj is :', obj);
+//address : {city: 'High-Tech', state: 'TS'}
 console.log('user is :', user);
+//address : {city: 'High-Tech', state: 'TS'}
 
 
 
-const originalArray = [1, 2, [3, 4]]   //change original array as well.So we use deep copy
+const originalArray = [1, 2, [3, 4]]   //change original array as well.So we use deep copy.
 const shallowCopy = [...originalArray]
 
 shallowCopy[2][0] = 99
 console.log(originalArray);
+// [1,2[99,4]]
+console.log(shallowCopy);
+// [1,2[99,4]]
