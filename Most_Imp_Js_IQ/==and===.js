3b3fd70bd19fab4665d0651bc9a12fa8972ba243
shaikhfurquan <shaikhfurquan313@gmail.com>
2023-11-19 15:49:05 +0530

    imp IQ

---
+++ Most_Imp_Js_IQ/==and===.js
@@ -0,0 +1,8 @@
+
+
+const  number1 = "10"
+const  number2 = 10
+//only check values
+console.log(number1 == number2);        //true  
+//check both  values and datatype as well
+console.log(number1 === number2);       //false
---
+++ Most_Imp_Js_IQ/typeof.js
@@ -0,0 +1,7 @@
+
+// typeof
+const Number = '10'
+let str = 'this is string'
+console.log(typeof(Number));    //string
+console.log(typeof(Number) === "string");    //true
+console.log(typeof(str) === "string");    //true
