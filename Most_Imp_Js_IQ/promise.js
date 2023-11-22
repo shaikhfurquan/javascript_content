ad21120622257c51e30b505d727e0616b554a8be
shaikhfurquan <shaikhfurquan313@gmail.com>
2023-11-22 13:59:48 +0530

    promise in js

---
+++ Most_Imp_Js_IQ/promise.js
@@ -0,0 +1,10 @@
+/*
+Promises
+defenition :- A promise in JavaScript is an object that represents the eventual completion of an asynchronous operation and its resulting value. It is used to handle asynchronous operations in a more predictable way.
+
+promise has three stage
+
+1.) Pending
+2.) Fulfilled -> .then  [resolve]
+3.) Reject   -> .catch  [reject]
+*/
