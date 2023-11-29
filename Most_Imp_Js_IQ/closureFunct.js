f76815eb78128ee92357c756edca1a063e819427
shaikhfurquan <shaikhfurquan313@gmail.com>
2023-11-29 15:40:35 +0530

    cluser function

--- Most_Imp_Js_IQ/closureFunct.js
+++ Most_Imp_Js_IQ/closureFunct.js
@@ -1,7 +1,7 @@
 //Closures in JavaScript.
 // defenition :- Closures are an ability of a function to remember the variables and functions that are declared in its outer scope.
 
-function random(){
+function msg(){
     let obj = {
         name:'sf',
         age:23
@@ -11,5 +11,27 @@ function random(){
     }
 }
 
-const response = random()
+const response = msg()
 response();
+
+
+
+function outerFunction() {
+    // Variable defined in the outer scope
+    let outerVariable = 'I am from the outer function';
+
+    // Inner function (closure)
+    function innerFunction() {
+        // Accessing the outerVariable from the outer scope
+        console.log(outerVariable);
+    }
+
+    // Returning the inner function
+    return innerFunction;
+}
+
+// Call the outer function and store the returned inner function in a variable
+const closureFunction = outerFunction();
+
+// Call the closure function, which still has access to outerVariable
+closureFunction();
