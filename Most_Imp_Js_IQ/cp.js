11df98e85316c2fb5cde5001465b3250f07882c7
shaikhfurquan <shaikhfurquan313@gmail.com>
2023-12-09 11:22:25 +0530

    changes

--- Most_Imp_Js_IQ/cp.js
+++ Most_Imp_Js_IQ/cp.js
@@ -1,2 +1,2 @@
 const cp = require('child_process')
-cp.exec('start chrome https://chat.openai.com/c/00a4d3e8-0263-4efb-a5b9-a905b398adb0')
+cp.exec('start chrome https://chat.openai.com/c/00a4d3e8-0263-4efb-a5b9-a905b398adb0') 
