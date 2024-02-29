de83ded9006fe6d0ef208796ee5a94057c4b8cfc
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-01 00:36:51 +0530

    largest array number

--- Most_Imp_Js_Programs/largestNumArray.js
+++ Most_Imp_Js_Programs/largestNumArray.js
@@ -10,3 +10,18 @@ const findLargestNumber = (nums) => {
   return largest;
 };
 console.log(findLargestNumber([4, 89, 4, 989, 46, 7, 4564, 654, 654]));
+
+
+
+const array = [10,200,30]
+let largestNumber = 0
+
+for(i=0 ; i<array.length ; i++){
+    if(array[i] > largestNumber){
+        largestNumber = array[i]
+    }
+    console.log("Array element==>" , array[i]);
+    console.log("largestNumber==>" , largestNumber);
+}
+
+console.log(`largest numberis ${largestNumber}`);
