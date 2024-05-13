16fe9607252ecb276c768df902058e50721e42f4
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-05-13 23:09:31 +0530

    removed duplicate element

--- Most_Imp_Js_Programs/removeDuplicateArray.js
+++ Most_Imp_Js_Programs/removeDuplicateArray.js
@@ -1,17 +1,22 @@
+// Program to remove duplicate element in an array
+
+// Ex-1
 const removeDuplicates = (arr) => {
     const uniqueArray = [...new Set(arr)];
     return uniqueArray;
-  };
-  console.log(removeDuplicates([1, 2, 3, 4, 3, 2, 1]));
-
-
-  let num = new Set([10,20,10])
-  console.log(num);
+};
+console.log(removeDuplicates([1, 2, 3, 4, 3, 2, 1]));
 
 
 
-  //Or
-  const removeDuplicate = (arr) => {
+// Ex-2
+let num = new Set([10, 20, 10])
+console.log(num);
+
+
+
+// Ex-3
+const removeDuplicate = (arr) => {
     let newUniqueArray = []
     for (num of arr) {
         if (!newUniqueArray.includes(num)) {
