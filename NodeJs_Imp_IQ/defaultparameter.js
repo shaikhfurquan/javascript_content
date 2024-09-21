05c059dae6d9bc35c4902172c353ba19dd527a66
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-09-21 21:28:25 +0530

    update folder and files

--- NodeJs_Imp_IQ/defaultparameter.js
+++
@@ -1,8 +0,0 @@
-
-let add = (a=10 , b=20) =>{
-    console.log(a+b);
-}
-//taking default values
-add()
-//passing the arguments
-add(40,40)
--- NodeJs_Imp_IQ/restparameter.js
+++
@@ -1,8 +0,0 @@
-let add = (...x) =>{
-    let sum = 0
-    for(let i=0; i<x.length; i++){
-        sum = sum + x[i]
-    }
-    console.log(sum);
-}
-add(4,4,6,6)
