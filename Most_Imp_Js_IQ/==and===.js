810fe257bf6e69fc80b4a9d6c50a7b1cb84fbdb3
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-09-24 13:32:26 +0530

    sttring vs boolean

--- Most_Imp_Js_IQ/==and===.js
+++ Most_Imp_Js_IQ/==and===.js
@@ -1,4 +1,16 @@
 
+console.log(1 == true); // true
+// The boolean `true` is coerced to the number `1`, so it becomes `1 == 1`, which is true.
+
+
+console.log('true' == true); // false
+// The string `'true'` is converted to NaN (Not-a-Number), and `true` is converted to 1. So, it's `NaN == 1`, which is false.
+
+
+console.log('123' == 123); // true
+// The string `'123'` is coerced to the number `123`, so it becomes `123 == 123`, which is true.
+
+
 
 const  number1 = "10"
 const  number2 = 10
