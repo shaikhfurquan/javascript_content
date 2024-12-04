a9f3a054b8b872ff02346b4974ce619b168168c3
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-12-04 18:23:06 +0530

    about variables

---
+++ Most_Imp_Js_IQ/let_var_const.js
@@ -0,0 +1,49 @@
+
+// var = block level scope and global scope    
+// let = block level scope
+// const = block level scope
+
+/*
+var Name = 'shaikh'
+const print = () =>{
+    var Name = 'sf';
+    console.log(Name);
+}
+
+print()
+var Name = 'MT'
+console.log(Name);
+//sf
+//MT
+*/
+
+/*
+let Name = 'raj'
+const print = () =>{
+    let Name = 'kumar';
+    console.log(Name);
+}
+print()
+Name = 'rj'
+console.log(Name);
+//kumar
+//rj
+*/
+
+/*
+const Name = 'sk'
+const print = () =>{
+    var Name = 'sf';
+    console.log(Name);
+}
+
+print()
+//  Name = 'MT'      not possible in const
+console.log(Name);
+//sf
+//sk
+*/
+
+
+
+
