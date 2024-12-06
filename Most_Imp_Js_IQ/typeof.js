643df43bf1cad5cc166611b13994718a9d75e89d
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-12-06 21:17:00 +0530

    typeof operator

---
+++ Most_Imp_Js_IQ/typeof.js
@@ -0,0 +1,10 @@
+/*
+TypeOf operator:-
+Typeof in JavaScript is an operator used for type checking & returns the data type of the operand passed to it.
+*/
+
+const Number = '10'
+let str = 'this is string'
+console.log(typeof(Number));                //string
+console.log(typeof(Number) === "string");   //true
+console.log(typeof(str) === "string");      //true
