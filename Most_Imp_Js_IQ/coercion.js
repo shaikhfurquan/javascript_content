8d3e3d0024c187eb5d93e2cdb9e613ecf3e4ec3a
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-12-23 12:23:58 +0530

    coercion

---
+++ Most_Imp_Js_IQ/coercion.js
@@ -0,0 +1,9 @@
+//Coercion in javascript.
+// defeniton :-  Implicit type coercion in javascript is the automatic conversion of value from one data type to another. It takes place when the operands of an expression are of different data types.
+
+
+
+let x=3
+let y = "5"
+console.log(x+y);   //string Coercion. --> 35
+console.log(x-y);   //number Coercion. --> -2
