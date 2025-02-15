ecedda614fad67a0ac3b20ac63b4843cae8586d1
shaikhfurquan <shaikhfurquan313@gmail.com>
2025-02-16 00:17:39 +0530

    sum of number

--- Most_Imp_Js_Programs/addArrayElement.js
+++ Most_Imp_Js_Programs/addArrayElement.js
@@ -1,12 +1,12 @@
 // Sum of array elements
 
 // Ex-1
-var arr = [1, 2, 3, 4, 5, 6]
-var sum = 0
+var arr = [1, 2, 3, 4, 5, 6];
+var sum = 0;
 
-arr.forEach((elem) => {
-    sum = sum + elem
-})
+for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
 console.log(sum); // 21
 
 
@@ -17,16 +17,18 @@ var sum = arr.reduce((acc, curVal) =>
 console.log(sum); // 21
 
 
-// Ex-2
-var arr = [1, 2, 3, 4, 5, 6];
-var sum = 0;
 
-for (var i = 0; i < arr.length; i++) {
-    sum += arr[i];
-}
+// Ex-3
+var arr = [1, 2, 3, 4, 5, 6]
+var sum = 0
+
+arr.forEach((elem) => {
+    sum = sum + elem
+})
 console.log(sum); // 21
 
 
+//Ex-4
 // Adding only numbers not string
 var arr = ['abc', 123, 'bdc', 4, 5, 'yq']
 var sum = 0
@@ -35,6 +37,5 @@ arr.forEach(function (elem) {
         sum = sum + elem
     }
 })
-
 console.log("Numbers sum only = ", sum);
 // Output: Numbers sum only = 132
