1918ed97477b4b1986c1f21141e4007d9907411e
shaikhfurquan <shaikhfurquan313@gmail.com>
2025-02-18 12:08:30 +0530

    first largest number

--- Most_Imp_Js_Programs/firstlargestNumArray.js
+++ Most_Imp_Js_Programs/firstlargestNumArray.js
@@ -1,5 +1,6 @@
-// Find the Largest Number in an Array:
+// Find the First/Largest Number in an Array:
 
+Ex-1
 const findLargestNumber = (nums) => {
   let largest = nums[0];
   for (let i = 0; i < nums.length; i++) {
@@ -9,19 +10,17 @@ const findLargestNumber = (nums) => {
   }
   return largest;
 };
-console.log(findLargestNumber([4, 89, 4, 989, 46, 7, 4564, 654, 654]));
+let arr = [4, 89, 4, 989, 46, 7, 4564, 654, 654]
+console.log("Largest Number is" ,findLargestNumber(arr));
 
 
-
+Ex-2
 const array = [10,200,30]
 let largestNumber = 0
-
 for(i=0 ; i<array.length ; i++){
     if(array[i] > largestNumber){
         largestNumber = array[i]
     }
-    console.log("Array element==>" , array[i]);
-    console.log("largestNumber==>" , largestNumber);
 }
 
 console.log(`largest numberis ${largestNumber}`);
